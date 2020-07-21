756094c00ff1081afaa481160b446f4ce0139c9a
j-ritenour <61949197+j-ritenour@users.noreply.github.com>
2020-07-21 18:26:48 -0500

    FINAL WITH UPDATED SQL QUERIES

--- queries.sql
+++ queries.sql
@@ -1,7 +1,19 @@
 ## Part 1: Test it with SQL
+id(int)
+employer VARCHAR
+skills VARCHAR
+name VARCHAR
 
 ## Part 2: Test it with SQL
+SELECT techjobs
+FROM employer
+WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
+Drop table job
 
 ## Part 4: Test it with SQL
+SELECT distinct name, description From Skill
+Inner join job_skills on skill.id = job_skills.skills_is
+WHERE jobs_id IS NOT NULL
+ORDER BY name ASC
